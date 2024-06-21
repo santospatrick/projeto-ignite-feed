5b698441a4851ca7a679e7b1ba91d1ee68b0681d
Patrick Santos <patrickspteixeira@gmail.com>
2024-06-20 21:57:23 -0300

    finaliza component sidebar e adiciona lib para icones

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,23 @@
 import styles from './sidebar.module.css'
+import { PencilLine } from "@phosphor-icons/react";
 
 function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-        <img className={styles.cover} src="https://images.unsplash.com/photo-1716278518129-3d803cc2460b?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img className={styles.cover} src="https://images.unsplash.com/photo-1716278518129-3d803cc2460b?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
         <div className={styles.profile}>
+            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/13510169?v=4" />
+
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
         </div>
 
         <footer>
-            <a href="#">Editar seu perfil</a>
+            <a href="#">
+                <PencilLine size={20} />
+                Editar seu perfil
+            </a>
         </footer>
     </aside>
   )
