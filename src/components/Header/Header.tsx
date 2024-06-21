bc1696fc2b505226330a04493a6ad056e92fe5ee
Patrick Santos <patrickspteixeira@gmail.com>
2024-06-20 21:28:16 -0300

    adiciona componente completo para header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
 import styles from './header.module.css'
+import Logo from '../../assets/logo.svg'
 
 function Header() {
   return (
-    <header className={styles.header}>Header</header>
+    <header className={styles.header}>
+        <img src={Logo} alt="Logotipo do ignite" />
+        <strong>Ignite Feed</strong>
+    </header>
   )
 }
 
